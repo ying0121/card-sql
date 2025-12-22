ecfb8d2126c734fb62b0dd33734354cd3e0a119a
ura <ura@gmail.com>
2025-12-22 15:40:22 +0900

    fixed: desc

--- migrations/2025-12-19_005_drop_column_eso_t_c0012_kaiheiki_table.sql
+++ migrations/2025-12-19_005_drop_column_eso_t_c0012_kaiheiki_table.sql
@@ -1,8 +1,8 @@
 
 -- 2025-12-19_005_drop_column_eso_t_c0012_kaiheiki.sql
 -- 目的：低圧開閉器(低圧)テーブルからSYMBOL_MUKIカラムを削除
--- 作成日: 2025-12-19
--- 作成者: 浦大輔
+-- 更新日: 2025-12-19
+-- 更新者: ikou2027
 -- 対象DB：PostgreSQL
 
 BEGIN;
